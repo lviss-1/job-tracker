02aae0800ef35ee0f8e786d9a6d37d87d222a006
lviss-1 <luispiece0@gmail.com>
2026-04-12 21:36:10 -0400

    Finished JwtUtil with token generation, extraction, and validation. Added extractEmail to parse and return subject claim from signed token. Added validateTOken to verify token integrity and email match via JwtException catch. Also added getSigningKey() helper to build secret key from Base-64 encoded secret.

--- backend/src/main/java/com/luis/job_tracker/security/JwtUtil.java
+++ backend/src/main/java/com/luis/job_tracker/security/JwtUtil.java
@@ -1,21 +1,21 @@
 package com.luis.job_tracker.security;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtUtil {
-    private static final String SECRET_KEY = "KEY";
+    private static final String SECRET_KEY = "FrqeBUJ97MOEgf7Lqg2PpZz3qdY+ujBK5P44hgDgfxg=";
     private static final int ACCESS_TOKEN_VALIDITY_SECONDS = 900;
     private static final int REFRESH_TOKEN_VALIDITY_SECONDS = 604800;
 
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
     }
 
     public String generateToken(String email) {
@@ -27,5 +27,23 @@ public class JwtUtil {
                 .expiration(expiryTime)
                 .signWith(getSigningKey())
                 .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String email) {
+        try {
+            String extractedEmail = extractEmail(token);
+            return extractedEmail.equals(email);
+        } catch(JwtException e) {
+            return false;
+        }
     }
 }
