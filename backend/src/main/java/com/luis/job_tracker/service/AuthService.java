15daac8beec0946b818728315a629a4d6dd3b506
lviss-1 <luispiece0@gmail.com>
2026-04-17 23:33:13 -0400

    Created AuthService.java which will serve as the class that does the register/login work for the app. Started by creating the constructor and injecting the four dependencies needed. Also added the loadUserByUsername method to get a users email and password.

---
+++ backend/src/main/java/com/luis/job_tracker/service/AuthService.java
@@ -0,0 +1,40 @@
+package com.luis.job_tracker.service;
+
+import com.luis.job_tracker.entity.User;
+import com.luis.job_tracker.repository.RefreshTokenRepository;
+import com.luis.job_tracker.repository.UserRepository;
+import com.luis.job_tracker.security.JwtUtil;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AuthService implements UserDetailsService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil, RefreshTokenRepository refreshTokenRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}
